6796ffb837a7a6e11e3ce1f0cd3e190f25acacb7
unknown <dungle160396@gmail.com>
2019-09-25 14:30:28 +0700

    [master] update alert message

--- cake_list.php
+++ cake_list.php
@@ -1,16 +1,16 @@
 <?php
-    include('dbConnect.php');
-    $sql = "SELECT * FROM moon_cake";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        echo "<select name='cake-data' onChange='showDetail(this.value)'>";
-		echo "<option value=''>Hãy chọn</option>";
-        while($row = $result->fetch_assoc()) {
-            echo "<option value=". $row["id"]. ">" . $row["name"]. "</option>";
-        }
-        echo "</select>";
-    } else {
-        echo "Không tìm thấy thông tin";
-    }
-    $conn->close();
+include('dbConnect.php');
+$sql = "SELECT * FROM moon_cake";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+echo "<select name='cake-data' onChange='showDetail(this.value)'>";
+echo "<option value=''>Hãy chọn</option>";
+while($row = $result->fetch_assoc()) {
+echo "<option value=". $row["id"]. ">" . $row["name"]. "</option>";
+}
+echo "</select>";
+} else {
+echo "Không tìm thấy thông tin";
+}
+$conn->close();
 ?>
--- index.php
+++ index.php
@@ -1,37 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Exercise 1</title>
-	<link rel="stylesheet" href="css/style.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Exercise 1</title>
+<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	<div class="cake-section">
-		<h2><strong>Bài 3: Ajax, PHP & MySQL - Ví dụ</strong></h2>
-		<form>
-			<p>Chọn hộp bánh trung thu:</p>
-			<?php
-				include('cake_list.php');
-			?>
-		</form>
-		<br>
-		<div id="cake-detail">
-			<p class="alert">Thông tin về hộp bánh sẽ được hiển thị ở đây</p>
-		</div>
-	</div>
-	<script>
-		function showDetail(data) {
-			var xmlhttp = new XMLHttpRequest();
-			xmlhttp.onreadystatechange = function() {
-				if (this.readyState == 4 && this.status == 200) {
-					document.getElementById("cake-detail").innerHTML = this.responseText;
-				}
-			}
-			xmlhttp.open("GET", "cake_detail.php?id="+data, true);
-			xmlhttp.send();
-		}
-	</script>
+<div class="cake-section">
+<h2><strong>Bài 3: Ajax, PHP & MySQL - Ví dụ</strong></h2>
+<form>
+<p>Chọn hộp bánh trung thu:</p>
+<?php
+include('cake_list.php');
+?>
+</form>
+<br>
+<div id="cake-detail">
+<p class="alert">Thông tin về hộp bánh sẽ được hiển thị ở đây</p>
+</div>
+</div>
+<script>
+function showDetail(data) {
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+if (this.readyState == 4 && this.status == 200) {
+document.getElementById("cake-detail").innerHTML = this.responseText;
+}
+}
+xmlhttp.open("GET", "cake_detail.php?id="+data, true);
+xmlhttp.send();
+}
+</script>
 </body>
 </html>
