bf5347203e3f4b00f96c21efa6a2949f1b3c3bfa
unknown <dungle160396@gmail.com>
2019-09-25 14:34:11 +0700

    [master] update

--- cake_detail.php
+++ cake_detail.php
@@ -1,16 +1,16 @@
 <?php
-$id = $_REQUEST["id"];
-if ($id != '') {
-include('dbConnect.php');
-$sql = "SELECT * FROM moon_cake where id = " . $id;
-$result = $conn->query($sql);
-while($row = $result->fetch_assoc()) {
-echo "<p>Tên bánh: <strong>" . $row['name'] . "</strong></p>";
-echo "<p>Giá bán: <strong>" . $row['price'] . "</strong> vnđ</p>";
-echo "<img src='images/" . $row['image'] . "' alt='Cake image'>";
-}
-$conn->close();
-} else {
-echo "<p class='alert'>Thông tin về hộp bánh sẽ được hiển thị ở đây</p>";
-}
+	$id = $_REQUEST["id"];
+	if ($id != '') {
+		include('dbConnect.php');
+		$sql = "SELECT * FROM moon_cake where id = " . $id;
+		$result = $conn->query($sql);
+		while($row = $result->fetch_assoc()) {
+			echo "<p>Tên bánh: <strong>" . $row['name'] . "</strong></p>";
+			echo "<p>Giá bán: <strong>" . $row['price'] . "</strong> vnđ</p>";
+			echo "<img src='images/" . $row['image'] . "' alt='Cake image'>";
+		}
+		$conn->close();
+	} else {
+		echo "<p class='alert'>Thông tin về hộp bánh sẽ được hiển thị ở đây</p>";
+	}
 ?>
--- dbConnect.php
+++ dbConnect.php
@@ -1,12 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "exercise1";
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "exercise1";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-mysqli_set_charset($conn,"utf8");
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    mysqli_set_charset($conn,"utf8");
 ?>
