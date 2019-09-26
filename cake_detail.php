63e0f15ed11268a4f27ef5294d78f7c0f7d105e0
unknown <dungle160396@gmail.com>
2019-09-26 11:47:07 +0700

    [master] fix comment

--- cake_detail.php
+++ cake_detail.php
@@ -1,12 +1,12 @@
 <?php
+	require_once('dbConnect.php');
 	$id = $_REQUEST["id"];
 	if ($id != '') {
-		include('dbConnect.php');
 		$sql = "SELECT * FROM moon_cake where id = " . $id;
 		$result = $conn->query($sql);
 		while($row = $result->fetch_assoc()) {
 			echo "<p>Tên bánh: <strong>" . $row['name'] . "</strong></p>";
-			echo "<p>Giá bán: <strong>" . $row['price'] . "</strong> vnđ</p>";
+			echo "<p>Giá bán: <strong>" . number_format($row['price']) . "</strong> vnđ</p>";
 			echo "<img src='images/" . $row['image'] . "' alt='Cake image'>";
 		}
 		$conn->close();
